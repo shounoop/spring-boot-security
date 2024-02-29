6ff3ad45a78b64610374e66f3fec8291f39cfc90
HMT <tuan.hm@tinasoft.com.vn>
2024-02-29 09:38:41 +0700

    update the security context holder

---
+++ src/main/java/com/shounoop/springbootsecurity/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.shounoop.springbootsecurity.config;
+
+import com.shounoop.springbootsecurity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
